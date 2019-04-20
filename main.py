fc06608cfe72701c40e76d3d4d3e0c46f80a8611
Nikita <hekntsnv@gmail.com>
2019-04-20 15:21:15 +0300

    some fixes #1

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import inflect
 from string import digits
 
 
-_symb_to_words = {"+":"plus", "-":"minus", "=":"equals", "/":"divide", "%":"percent", "*":"multiplicate", "(":"left bracket", ")":"right bracket", "^":"raise to the power of"}
+_symb_to_word = {"+":"plus", "-":"minus", "=":"equals", "/":"divide", "%":"percent", "*":"multiplicate", "(":"left bracket", ")":"right bracket", "^":"raise to the power of"}
 
 def is_valid(exp_list):
-    if (set(exp_list).intersection(f"{digits} {''.join(_symb_to_words.keys())}") and 
+    if (set(exp_list).issubset(f"{digits} {''.join(_symb_to_word.keys())}") and 
             all(elem.isdigit() or not set(elem).intersection(f"{digits}") for elem in exp_list)):
         return True
     else:
@@ -16,5 +16,5 @@ def math_to_str(expression):
     if not is_valid(exp_lst):
         return "invalid input"
     eng = inflect.engine()
-    return ' '.join([eng.number_to_words(elem) if elem.isdigit() else _symb_to_words[elem] for elem in exp_lst])
+    return ' '.join([eng.number_to_words(elem) if elem.isdigit() else _symb_to_word[elem] for elem in exp_lst])
 
