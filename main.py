f60fc2406198a63122e1f665f09374e5be37459b
Nikita <hekntsnv@gmail.com>
2019-04-23 23:52:52 +0300

    completely working own version without dependent libraries

--- main.py
+++ main.py
@@ -1,20 +1,64 @@
-import inflect
 from string import digits
 
+_digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+_from_ten_to_twenty = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'] 
+_hundredths = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+_num_orders = ['thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion']
 
 _symb_to_word = {"+":"plus", "-":"minus", "=":"equals", "/":"divide", "%":"percent", "*":"multiplicate", "(":"left bracket", ")":"right bracket", "^":"raise to the power of"}
 
-def is_valid(exp):
+def is_valid(exp: str):
     if (set(exp).issubset(f"{digits} {''.join(_symb_to_word.keys())}") and 
             all(elem.isdigit() or not set(elem).intersection(f"{digits}") for elem in exp.split())):
         return True
     else:
         return False
-    
-def math_to_str(expression):
+
+def concat_order(ord_num: int, order: str) -> str:
+    res = ''
+    ord_len = len(order)
+    if ord_len == 1:
+        res += _digits[int(order[0])]
+    elif ord_len == 2 or ord_len == 3:
+        if ord_len == 3 and order[0] != '0':
+            res += f'{_digits[int(order[0])]} hundred'
+            if order[0] != '0' or order[1] != '0':
+               res += ' and '
+        if order[-2] == '1':
+            res += _from_ten_to_twenty[int(order[-1])]
+        elif order[-2] == '0' and order[-1] != '0':
+            res += _digits[int(order[-1])]
+        elif order[-2] not in '01':
+            res += _hundredths[int(order[-2])-2]
+            if order[-1] != '0':
+                res += f'-{_digits[int(order[-1])]}'
+    if ord_num and set(order) != {'0'}:
+        res += f' {_num_orders[ord_num-1]}'
+    return res
+
+def orders_split(exp: str) -> list:
+    l = [exp[i-3:i] for i in range(len(exp),0,-3)]
+    if len(exp)%3:
+        l[-1] = exp[:len(exp)%3]
+    return l
+
+
+def math_to_str(expression: str) -> str:
     if not is_valid(expression):
         return "invalid input"
-    exp_lst = expression.split()
-    eng = inflect.engine()
-    return ' '.join([eng.number_to_words(elem) if elem.isdigit() else _symb_to_word[elem] for elem in exp_lst])
+    res = ''
+    for elem in expression.split():
+        if res:
+            res += ' '
+        if elem.isdigit():
+            ord_elem = orders_split(elem.lstrip("0"))
+            res_dig_elem = '' if ord_elem else 'zero'
+            for num,el in enumerate(ord_elem):
+                c_o = concat_order(num,el)
+                if c_o:
+                    res_dig_elem = f'{c_o}, {res_dig_elem}' if num and res_dig_elem else c_o + res_dig_elem
+            res += res_dig_elem
+        else:
+            res += _symb_to_word[elem]
+    return res
 
--- main_test.py
+++ main_test.py
@@ -5,11 +5,14 @@ from main import math_to_str as mts
 def test_answer():
     assert mts('1 + 3 = 4') == 'one plus three equals four'
     assert mts('1+ 3 = 4') == 'invalid input'
-    assert mts('') == ''
+    assert mts('1.4') == 'invalid input'
+    assert mts('1,4') == 'invalid input'
+    assert mts(' ') == ''
     assert mts('ir232 =') == 'invalid input'
     assert mts('11 + 3 = 4') == 'eleven plus three equals four'
     assert mts('= ( ) + - 312 4 1 * ^ / 0') == 'equals left bracket right bracket plus minus three hundred and twelve four one multiplicate raise to the power of divide zero'
     assert mts('   +   ') == 'plus'
     assert mts('8   10   ') == 'eight ten'
     assert mts('724209') == 'seven hundred and twenty-four thousand, two hundred and nine'
+    assert mts('123456789123456789123456789123456789') == 'one hundred and twenty-three decillion, four hundred and fifty-six nonillion, seven hundred and eighty-nine octillion, one hundred and twenty-three septillion, four hundred and fifty-six sextillion, seven hundred and eighty-nine quintillion, one hundred and twenty-three quadrillion, four hundred and fifty-six trillion, seven hundred and eighty-nine billion, one hundred and twenty-three million, four hundred and fifty-six thousand, seven hundred and eighty-nine'
 
